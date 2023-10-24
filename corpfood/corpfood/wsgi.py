218da50b49bc9a0b6ed366e9fedc16894623658d
Slava14tuv <aldarlamazhyk@yandex.ru>
2023-10-24 20:48:04 +0700

    add corpfoodapp and one route

---
+++ corpfood/corpfood/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for corpfood project.
+
+It
+xposes the WSGI callable as a module-level variable named ``application``.e
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corpfood.settings')
+
+application = get_wsgi_application()
